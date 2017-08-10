646043170a3c07c2daea8527f00562b332148041
vtripeno <victoryuri91@hotmail.com>
2017-08-09 22:42:34 -0300

    JSON OBJECT

--- src/main/java/br/com/code/sorcerers/spring_boot_api_financial/FincancialController.java
+++ src/main/java/br/com/code/sorcerers/spring_boot_api_financial/FincancialController.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -11,6 +13,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.SerializationUtils;
@@ -26,23 +29,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class FincancialController {
 
 	@RequestMapping("/consultScore")
-	public ResponseEntity<Financial> consultScore(@RequestBody Financial financial) {
+	public HttpResponse consultScore(@RequestBody Financial financial) {
 		
-		financial.setId(1);
+		/*financial.setId(1);
 		financial.setName("NAME TESTE");
 		financial.setCredit(50000.00);
-		financial.setAccount("1234");
+		financial.setAccount("1234");*/
+
+		Map<String, Financial> map = new HashMap<String, Financial>();
+		map.put("finance",financial);
 		
-		
-		HttpClient httpClient = HttpClientBuilder.create().build(); 
+		HttpClient httpClient = HttpClientBuilder.create().build();
+
+		HttpResponse response = null;
 
 		try {
 
 		    HttpPost request = new HttpPost("http://localhost:9090/socket");
 		    request.addHeader("content-type", "application/json");
-		    StringEntity params =new StringEntity("details={\"name\":\"myname\",\"age\":\"20\"} ");
+			JSONObject json = new JSONObject(map);
+		    StringEntity params = new StringEntity(json.toString());
 		    request.setEntity(params);
-		    HttpResponse response = httpClient.execute(request);
+		    response = httpClient.execute(request);
 
 		    //handle response here...
 
@@ -54,7 +62,7 @@ public class FincancialController {
 		    httpClient.getConnectionManager().shutdown(); //Deprecated
 		}
 
-		return new ResponseEntity<Financial>(financial, HttpStatus.OK);
+		return response;
 	}
 
 }
