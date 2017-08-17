813143426f6cec13ef54d0b01685658ed09c03c6
vrto <victor.tripeno@gft.com>
2017-08-17 17:48:49 -0300

    RestTemplate

--- src/main/java/br/com/code/sorcerers/spring_boot_api_financial/FincancialController.java
+++ src/main/java/br/com/code/sorcerers/spring_boot_api_financial/FincancialController.java
@@ -14,9 +14,12 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -29,45 +32,14 @@ public class FincancialController {
 	@RequestMapping("/consultScore")
 	public String consultScore(@RequestBody Financial financial) {
 		
-
-		Map<String, Financial> map = new HashMap<String, Financial>();
-		map.put("finance",financial);
-		
-		StringBuffer result = new StringBuffer();
-		
 		String uri = "http://localhost:9090/socket";
-
-		HttpClient client = new DefaultHttpClient();
-
-		try {
-
-			JSONObject json = new JSONObject(map);
-		    StringEntity params = new StringEntity(json.toString());
-
-		    URL url = new URL(uri);
-			HttpPost post = new HttpPost(url.toString());
-
-			params.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-			post.setEntity(params);
-
-			HttpResponse response = client.execute(post);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-			
-			System.out.println(response.toString());
-
-		}catch (Exception ex) {
-
-
-		} finally {
-		    client.getConnectionManager().shutdown(); 
-		}
-
-		return result.toString();
+		
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		HttpEntity<Financial> request = new HttpEntity<Financial>(financial);
+		
+		return restTemplate.postForObject(uri, request, String.class);
+		
 	}
 	
 }
