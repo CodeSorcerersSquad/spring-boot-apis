e2e2968d2999f06b0aa13738dfb4dbfba5a98fa8
vtripeno <victoryuri91@hotmail.com>
2017-08-25 22:09:21 -0300

    Code adjust

--- src/main/java/br/com/code/sorcerers/spring_boot_api_financial/App.java
+++ src/main/java/br/com/code/sorcerers/spring_boot_api_financial/App.java
@@ -13,8 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
+
         SpringApplication.run(App.class, args);
     }
 
